230fd3d20ae6bfd5ef5d4082d04cc749e7592a88
BionicHawk <angel.manzo.rosas@gmail.com>
2024-05-01 16:47:34 -0600

    Agregando método para cubrir accidentes vehículares

--- source/models/polizas.py
+++ source/models/polizas.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from .registro import Registro
 
 
 class Poliza:
@@ -11,13 +12,32 @@ class Poliza:
 class Vehicular(Poliza):
     marca: str
     modelo: str
+    titular: str
+    registros: list[Registro]
+
+    def intentar_cubrir_daños_accidentes(self, razon: str, dias_de_vencimiento: int, monto_a_regresar: float = 0.0) -> bool:
+        if dias_de_vencimiento < 1 or monto_a_regresar == 0.0 or len(razon) == 0:
+            return False
+
+        today = datetime.now()
+
+        self.registros.append(Registro(
+            razon=razon,
+            expedicion=today,
+            monto=monto_a_regresar,
+            vencimiento=today + timedelta(days=dias_de_vencimiento)
+        ))
+
+        return True
 
 
 class Inmueble(Poliza):
     direccion: str
     area: float
+    titular: str
 
 
 class Adicional(Poliza):
     titulo: str
     descripcion: str
+    titular: str
